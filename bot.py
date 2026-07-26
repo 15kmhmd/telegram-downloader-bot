c54d852ce96dea2b37b194bcbcf4cdbee5a135f7
15kmhmd <mhmdmdnwn34@gmail.com>
2026-07-26 23:22:12 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,31 @@
 import asyncio
-from aiogram import Bot, Dispatcher
-from config import BOT_TOKEN
+import logging
 
-bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher()
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+
+from config import BOT_TOKEN
+from handlers import start_router, download_router, admin_router
+
+logging.basicConfig(level=logging.INFO)
 
 async def main():
-    print("Bot Started...")
+    bot = Bot(
+        token=BOT_TOKEN,
+        default=DefaultBotProperties(
+            parse_mode=ParseMode.HTML
+        )
+    )
+
+    dp = Dispatcher()
+
+    dp.include_router(start_router)
+    dp.include_router(download_router)
+    dp.include_router(admin_router)
+
+    print("🚀 Abu Janat Downloader Started...")
+
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
