605b153fc213c5e97a6449923a1aacbc883918bf
15kmhmd <mhmdmdnwn34@gmail.com>
2026-07-26 22:55:55 +0300

    Refactor BOT_TOKEN to use os.getenv
    Updated BOT_TOKEN to use environment variable.

--- config.py
+++ config.py
@@ -1,1 +1,13 @@
-BOT_TOKEN = "7724138715:AAFoH44K9BF5NFqNzPiwZdc67RGvTZPUvSA"
+import os
+
+BOT_TOKEN = os.getenv("7724138715:AAFoH44K9BF5NFqNzPiwZdc67RGvTZPUvSA")
+
+OWNER_ID = 900438535  # ضع معرف تيليجرام الخاص بك هنا
+
+BOT_NAME = "Abu Janat Downloader"
+
+BOT_VERSION = "1.0.0"
+
+CHANNEL_URL = "https://t.me/YourChannel"
+
+DEVELOPER_URL = "https://t.me/YourUsername"
