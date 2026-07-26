373a6c389bd901179182dfd579142bedbaa91432
15kmhmd <mhmdmdnwn34@gmail.com>
2026-07-26 23:04:08 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
+
 import os
 
 BOT_TOKEN = os.getenv("7724138715:AAFoH44K9BF5NFqNzPiwZdc67RGvTZPUvSA")
 
-OWNER_ID = 900438535  # ضع معرف تيليجرام الخاص بك هنا
+OWNER_ID = 900438535
 
 BOT_NAME = "Abu Janat Downloader"
-
 BOT_VERSION = "1.0.0"
 
 CHANNEL_URL = "https://t.me/YourChannel"
-
-DEVELOPER_URL = "https://t.me/YourUsername"
+DEVELOPER_USERNAME = "@M59_3"
+DEVELOPER_URL = "https://t.me/M59_3"
