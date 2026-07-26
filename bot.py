e785820e086b42863ff7f2d0012c4f3de7f017d7
15kmhmd <mhmdmdnwn34@gmail.com>
2026-07-26 22:27:16 +0300

    Initialize bot with aiogram and start polling

---
+++ bot.py
@@ -0,0 +1,13 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from config import BOT_TOKEN
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+async def main():
+    print("Bot Started...")
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
