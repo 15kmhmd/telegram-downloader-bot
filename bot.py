49232f84032a61fc66993e8774695af0d9448414
15kmhmd <mhmdmdnwn34@gmail.com>
2026-07-27 19:44:20 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,9 +3,15 @@ import logging
 
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
-from aiogram.enums import ParseMo
+from aiogram.enums import ParseMode
+
+from config import BOT_TOKEN
+from handlers.start import router as start_router
+from handlers.download import router as download_router
+from handlers.admin import router as admin_router
 
 logging.basicConfig(level=logging.INFO)
+
 
 async def main():
     bot = Bot(
@@ -24,6 +30,7 @@ async def main():
     print("🚀 Abu Janat Downloader Started...")
 
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
